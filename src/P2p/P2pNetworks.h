fd63360f6ebb370abd56cafb4902183632b144b0
root <root@Fepecoin1.2.net>
2017-06-19 12:06:34 -0700

    first commit

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x44, 0x42, 0x49, 0x40, 0x55, 0x5f, 0x42, 0x8f, 0x84, 0x85, 0x80, 0x72, 0x60, 0x31, 0x31, 0x20 } };
+}
